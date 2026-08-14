1168df1748034d3b4003b10591fce57c66855895
wiranatadimass11 <wiranatadimass11@gmail.com>
2026-08-14 10:08:13 +0800

    add SIPKO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'SIKATAR — Sistem Informasi Kendaraan Kantor SAR Tarakan',
+  title: 'SIPKO — Sistem Informasi Pencatatan Kendaraan Operasional',
   description: 'Pencatatan kendaraan operasional SAR',
 };
 
